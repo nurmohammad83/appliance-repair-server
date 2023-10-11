5fb0bb2ac19b6b1339b692175265c375660fdd9f
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-12 00:18:55 +0600

    feat: added common generic respons functionality

---
+++ src/interfaces/common.ts
@@ -0,0 +1,17 @@
+import { IGenericErrorMessage } from './error';
+
+export type IGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorMessages: IGenericErrorMessage[];
+};
+
+export type IGenericResponse<T> = {
+  meta: {
+    page: number;
+    size: number;
+    total: number;
+    totalPage: number;
+  };
+  data: T;
+};
