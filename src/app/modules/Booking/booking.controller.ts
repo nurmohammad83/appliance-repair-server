248fa636766c76b27b013a51fa6ecd58bb960800
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-12 21:25:20 +0600

    feat: added booking service controller

---
+++ src/app/modules/Booking/booking.controller.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import tryAsync from '../../../shared/tryAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { BookingService } from './booking.service';
+
+const insertIntoDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await BookingService.insertIntoDb(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Booking created successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const getAllFromDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await BookingService.getAllFromDb();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Categories fetched successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const getByIdFromDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await BookingService.getByIdFromDb(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Booking fetched successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const updateByIdFromDb = tryAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+  const result = await BookingService.updateByIdFromDb(id, data);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Booking updated successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const deleteByIdFromDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await BookingService.deleteByIdFromDb(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Booking deleted successfully',
+    success: true,
+    data: result,
+  });
+});
+
+export const BookingController = {
+  getAllFromDb,
+  getByIdFromDb,
+  updateByIdFromDb,
+  deleteByIdFromDb,
+  insertIntoDb,
+};
