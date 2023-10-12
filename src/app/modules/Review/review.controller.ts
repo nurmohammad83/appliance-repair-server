b02441eb7790a45cca06d283ff0bb0f6e8076cd8
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-12 23:38:01 +0600

    feat: added reivew controller

---
+++ src/app/modules/Review/review.controller.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express';
+
+import httpStatus from 'http-status';
+import tryAsync from '../../../shared/tryAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { ReviewService } from './review.service';
+
+const insertIntoDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await ReviewService.insertIntoDb(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Review & Rating created successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const getAllFromDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await ReviewService.getAllFromDb();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Reviews fetched successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const getByIdFromDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await ReviewService.getByIdFromDb(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Review & Rating fetched successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const updateByIdFromDb = tryAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+  const result = await ReviewService.updateByIdFromDb(id, data);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Review & Rating updated successfully',
+    success: true,
+    data: result,
+  });
+});
+
+const deleteByIdFromDb = tryAsync(async (req: Request, res: Response) => {
+  const result = await ReviewService.deleteByIdFromDb(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    message: 'Review & Rating deleted successfully',
+    success: true,
+    data: result,
+  });
+});
+
+export const ReviewController = {
+  getAllFromDb,
+  getByIdFromDb,
+  updateByIdFromDb,
+  deleteByIdFromDb,
+  insertIntoDb,
+};
