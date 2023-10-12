c0971005abff522c8717b68b2a9f85d484b61b7b
Nur Mohammad <mohammadnur5480@gmail.com>
2023-10-12 16:03:00 +0600

    feat: added service route

---
+++ src/app/modules/services/services.route.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import { ServicesController } from './services.controller';
+
+const router = express.Router();
+
+router.post(
+  '/',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  ServicesController.insertIntoDb
+);
+
+router.get('/', ServicesController.getAllFromDb);
+router.get('/:id', ServicesController.getByIdFromDb);
+
+// router.delete(
+//   '/:id',
+//   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+//   ServicesController.deleteByIdFromDb
+// );
+
+// router.patch(
+//   '/:id',
+//   auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+//   ServicesController.updateByIdFromDb
+// );
+
+export const CategoryRoutes = router;
